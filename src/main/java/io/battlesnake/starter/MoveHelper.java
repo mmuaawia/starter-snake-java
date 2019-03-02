23b496b6fc58d628c650a36ae309faf0f74a23a8
amuaawia <amuaawia@abebooks.com>
2019-03-01 19:22:59 -0800

    heroku testing

--- src/main/java/io/battlesnake/starter/MoveHelper.java
+++ src/main/java/io/battlesnake/starter/MoveHelper.java
@@ -27,7 +27,9 @@ public class MoveHelper {
 
 
   public static String getMove(Position position, Board board) {
+    if (board.foods.isEmpty()) return shitMove(position, board);
     List<PositionNode> closeFoods = getCloseFoods(board.foods, position, Integer.MAX_VALUE);
+    //if
     Position bestFood = bestFood(closeFoods, board);
     String move = bfs(position, bestFood, board);
     return move == null ? shitMove(position, board) : move ;
