01fa2c7e95072a5edfb67d5b85f1ecd06d1003fa
amuaawia <amuaawia@abebooks.com>
2019-03-02 11:50:55 -0800

    enable debug logs

--- src/main/java/io/battlesnake/starter/SnakeApplication.java
+++ src/main/java/io/battlesnake/starter/SnakeApplication.java
@@ -24,7 +24,7 @@ public class SnakeApplication {
   private static final Logger LOG = LoggerFactory.getLogger(SnakeApplication.class);
   private static Board board;
   static int health;
-  private static boolean doLogging = false;
+  private static boolean doLogging = true;
 
   /**
    * Main entry point.
