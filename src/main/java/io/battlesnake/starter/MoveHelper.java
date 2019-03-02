42b472145033bb6753daa1d4c943b6d64b729a48
kbouwman <kbouwman@abebooks.com>
2019-03-02 14:57:45 -0800

    made killmove global

--- src/main/java/io/battlesnake/starter/MoveHelper.java
+++ src/main/java/io/battlesnake/starter/MoveHelper.java
@@ -87,7 +87,7 @@ public class MoveHelper {
         }
       }
     }
-    if(SnakeApplication.doLogging){LOG.info("Moving into direction: "+bestCase.toString()+" because amount of spaces found was not good enough: "+ bestAmountOfSpace);}
+    if(bestCase != null && SnakeApplication.doLogging){LOG.info("Moving into direction: "+bestCase.toString()+" because amount of spaces found was not good enough: "+ bestAmountOfSpace);}
     return bestCase;
   }
 
@@ -208,14 +208,11 @@ public class MoveHelper {
   }
 
   public static Move returnKillMove(Position position, Board board){
-    if(!SnakeApplication.Handler.youAreAlpha(board)){
-      return null;
-    }
     for (Move move : Move.values()) {
       if (MoveHelper.isMoveValid(position, move, board)) {
         Position proposedPosition = position.move(move);
-        for(Position enemyHead : board.enemyHeads){
-          if(Move.isPositionOneMoveAway(proposedPosition,enemyHead)){
+        for(int index = 0 ; index < board.enemyHeads.size(); index++){
+          if(Move.isPositionOneMoveAway(proposedPosition,board.enemyHeads.get(index)) && board.ourLength > board.enemyLengths.get(index)){
             if(SnakeApplication.doLogging){LOG.info("KILL MOVE FOUND");}
             return move;
           }
