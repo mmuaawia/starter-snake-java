8093176c719062044051008606d0e073b1cf1e5f
amuaawia <amuaawia@abebooks.com>
2019-03-01 18:51:27 -0800

    Naive Approach Going Fingers crossed

--- src/main/java/io/battlesnake/starter/MoveHelper.java
+++ src/main/java/io/battlesnake/starter/MoveHelper.java
@@ -1,11 +1,13 @@
 package io.battlesnake.starter;
 
 import javafx.geometry.Pos;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.*;
 
 public class MoveHelper {
-
+  private static final Logger LOG = LoggerFactory.getLogger(MoveHelper.class);
 
   public static boolean isMoveValid(Position position, Move move, Board board) {
     Position newPosition = position.move(move);
@@ -26,6 +28,15 @@ public class MoveHelper {
 
 
   public static String getMove(Position position, Board board) {
+    List<PositionNode> closeFoods = getCloseFoods(board.foods, position, board.height);
+    Position bestFood = bestFood(closeFoods, board);
+    String move = bfs(position, bestFood, board);
+    return move == null ? shitMove(position, board) : move ;
+
+
+  }
+
+  public static String shitMove (Position position, Board board) {
     List<Move> validMoves = new ArrayList<>();
     for (Move move : Move.values()) {
       if (MoveHelper.isMoveValid(position, move, board)) {
@@ -38,7 +49,39 @@ public class MoveHelper {
     return validMoves.get(index).toString();
   }
 
-  public static Position bestFood(PositionNode[] foods, Board board) {
+  private static String bfs(Position src, Position dest, Board board) {
+    boolean[][] visited = new boolean[board.height][board.width];
+    Queue<PositionNode> q = new LinkedList<>();
+    PositionNode srcNode = (PositionNode) src;
+    srcNode.initMove = null;
+    q.add(srcNode);
+    String closestFoodMove = null;
+    while (!q.isEmpty()) {
+      PositionNode currPos = q.poll();
+      visited[currPos.y][currPos.x] = true;
+      if (board.grid[currPos.y][currPos.x] == 1) {
+        closestFoodMove = currPos.initMove.toString();
+      }
+      if (dest.y == currPos.y && dest.x == currPos.x) {
+        return currPos.initMove.toString();
+      }
+
+      for (Move move : Move.values()) {
+        if (isMoveValid(currPos, move, board)) {
+          PositionNode nextPos = (PositionNode) currPos.move(move);
+          nextPos.initMove = currPos.initMove == null ? move : currPos.initMove;
+          if (!visited[nextPos.y][nextPos.x])
+            q.add(nextPos);
+        }
+      }
+    }
+    return closestFoodMove;
+
+
+
+  }
+
+  public static Position bestFood(List<PositionNode> foods, Board board) {
     int[][] grid = board.grid;
     int bestFoodIndex = 0;
     int greatestDist = Integer.MIN_VALUE;
@@ -74,9 +117,7 @@ public class MoveHelper {
       i++;
 
     }
-
-
-    return foods[bestFoodIndex];
+    return foods.get(bestFoodIndex);
   }
 
   public static ArrayList<PositionNode> getCloseFoods(ArrayList<PositionNode> foods, Position position, int range){
@@ -89,8 +130,5 @@ public class MoveHelper {
     return closerFoods;
   }
 
-  public static Move getMoveToClosestFood(int x, int y, Board board) {
-    return null;
-  }
 
 }
--- src/main/java/io/battlesnake/starter/PositionNode.java
+++ src/main/java/io/battlesnake/starter/PositionNode.java
@@ -2,9 +2,11 @@ package io.battlesnake.starter;
 
 public class PositionNode extends Position {
   public int distance;
+  public Move initMove = Move.LEFT;
 
   PositionNode(Position position, int distance) {
     super(position.x, position.y);
     this.distance = distance;
+
   }
 }
